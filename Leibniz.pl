56f04b932ff92de8881db588766bf23f34771866
Anthony John Ripa <you@example.com>
2018-10-20 18:23:38 -0400

    Recast problem from an optimization problem to a search problem.

--- Leibniz.pl
+++ Leibniz.pl
@@ -1,6 +1,6 @@
 
 %	Author:		Anthony John Ripa
-%	Date:		2018.09.20
+%	Date:		2018.10.20
 %	Leibniz:	A Rule System for Math
 
 :- op(0800,xfx,@).
@@ -19,51 +19,53 @@ preprocess(X/Y,X1/Y1) :- preprocess(X,X1) , preprocess(Y,Y1) , ! .
 preprocess(X@Y,X1@Y1) :- preprocess(X,X1) , preprocess(Y,Y1) , ! .
 preprocess(X,X) :- ! .
 
-simp(X/X, 1, S) :- writeln([S,9,X/X=1,trying]) , S>0 , X\=0 , ! , writeln([S,9,X/X=1,succeed]).
+simp(X/X, 1) :- show([9,X/X=1,trying]) , X\=0 , ! , show([9,X/X=1,succeed]).
 
-simp(X*1, Ans, S) :- writeln([S,11,X*1=Ans,trying]) , S>0 , S2 is S-1 , simp(X, Ans, S2) , ! , writeln([S,11,X,succeed]).
-simp(1*X, Ans, S) :- writeln([S,12,X]) , S>0 , S2 is S-1 , simp(X, Ans, S2) , ! , writeln([S,12,X,succeed]).
-simp(X*0, 0, S) :- writeln([S,13,X]) , S>0 , ! , writeln([S,13,X,succeed]).
-simp(0*X, 0, S) :- writeln([S,14,X]) , S>0 , ! , writeln([S,14,X,succeed]).
-simp(A*B, Ans, S) :- writeln([S,15,A,B]) , S>0, S2 is S-1 , simp(A, A1, S2) , A\=A1 , simp(A1*B, Ans, S2) , ! , writeln([S,15,A,B,succeed]).
-simp(A*B, Ans, S) :- writeln([S,16,A,B]) , S>0 , S2 is S-1 , simp(B, B1, S2) , B\=B1 , simp(A*B1, Ans, S2) , ! , writeln([S,16,A,B,succeed]).
+simp(X*1, Ans) :- show([11,X*1=Ans,trying]) , simp(X, Ans) , ! , show([11,X,succeed]).
+simp(1*X, Ans) :- show([12,X]) , simp(X, Ans) , ! , show([12,X,succeed]).
+simp(X*0, 0) :- show([13,X]) , ! , show([13,X,succeed]).
+simp(0*X, 0) :- show([14,X]) , ! , show([14,X,succeed]).
+simp(A*B, Ans) :- show([15,A,B]) , simp(A, A1) , A\=A1 , simp(A1*B, Ans) , ! , show([15,A,B,succeed]).
+simp(A*B, Ans) :- show([16,A,B]) , simp(B, B1) , B\=B1 , simp(A*B1, Ans) , ! , show([16,A,B,succeed]).
 
-simp(X+0, Ans, S) :- writeln([S,18,X]) , S>0 , S2 is S-1 , simp(X, Ans, S2) , ! .
-simp(0+X, Ans, S) :- writeln([S,19,X]) , S>0 , S2 is S-1 , simp(X, Ans, S2) , ! .
-simp(A+B, Ans, S) :- writeln([S,20,A,B]) , S>0 , S2 is S-1 , simp(A, A1, S2) , A\=A1 , simp(A1+B, Ans, S2) , ! .
-simp(A+B, Ans, S) :- writeln([S,21,A,B]) , S>0 , S2 is S-1 , simp(B, B1, S2) , B\=B1 , simp(A+B1, Ans, S2) , ! .
-simp(C*(A+B), Ans, S) :- writeln([S,22,A,B,C]) , S>0 , S2 is S-1 , simp(C*A+C*B, Ans, S2) , ! .
-simp((A+B)*C, Ans, S) :- writeln([S,23,A,B,C]) , S>0 , S2 is S-1 , simp(A*C+B*C, Ans, S2) , ! .
+simp(X+0, Ans) :- show([18,X]) , simp(X, Ans) , ! .
+simp(0+X, Ans) :- show([19,X]) , simp(X, Ans) , ! .
+simp(A+B, Ans) :- show([20,A,B]) , ground(A) , simp(A, A1) , A\=A1 , simp(A1+B, Ans) , ! .
+simp(A+B, Ans) :- show([21,A,B]) , ground(B) , simp(B, B1) , B\=B1 , simp(A+B1, Ans) , ! .
+simp(C*(A+B), Ans) :- show([22,A,B,C]) , simp(C*A+C*B, Ans) , ! .
+simp((A+B)*C, Ans) :- show([23,A,B,C]) , simp(A*C+B*C, Ans) , ! .
 
-simp(A+B-C, Ans, S) :- writeln([S,25,A,B,C]) , S>0 , S2 is S-1 , simp(B,B1,S2) , simp(C,C1,S2) , B1=C1 , simp(A, Ans, S2) , ! .
-simp(A+B-C, Ans, S) :- writeln([S,26,A,B,C]) , S>0 , S2 is S-1 , simp(A-C,Z,S2) , simp(Z+B, Ans, S2) , ! .
-simp(A-B, Ans, S) :- writeln([S,27,A,B]) , S>0 , S2 is S-1 , simp(A, A1+A2,S2) , simp(A1+A2-B,Ans,S2) , ! .
-simp(A-B, Ans, S) :- writeln([S,28,A,B]), S>0 , S2 is S-1 , simp(A, A1, S2) , simp(B,B1,S2) , simp(B1+Ans, A1,S2) , ! .
-simp(A-B, Ans, S) :- writeln([S,29,A,B]) , S>0 , S2 is S-1 , simp(A, A1, S2) , simp(B,B1,S2) , simp(Ans+B1, A1,S2) , ! .
+simp(A+B-C, Ans) :- show([25,A,B,C]) , simp(B,B1) , simp(C,C1) , B1=C1 , simp(A, Ans) , ! .
+simp(A+B-C, Ans) :- show([26,A,B,C]) , simp(A-C,Z) , simp(Z+B, Ans) , ! .
+simp(A-B, Ans) :- show([28,A,B]) , simp(A, A1+A2) , simp(A1+A2-B,Ans) , ! .
+simp(A-B, Ans) :- show([29,A,B]) , simp(A, A1) , simp(B,B1) , simp(B1+Ans, A1) , ! .
+simp(A-B, Ans) :- show([30,A,B]) , simp(A, A1) , simp(B,B1) , simp(Ans+B1, A1) , ! .
 
-simp(A*B/C, Ans, S) :- writeln([S,34,A]) , S>0 , S2 is S-1 , simp(B,B1,S2) , simp(C,C1,S2) , B1=C1 , simp(A, Ans, S2) , ! .
-simp(A*B/C, Ans, S) :- writeln([S,35,A]) , S>0 , S2 is S-1 , simp(A/C,Z,S2) , simp(Z*B, Ans, S2) , ! .
-simp(A/B, Ans, S) :- writeln([S,37,A,B]) , S>0 , S2 is S-1 , simp(A,A1,S2) , A\=A1 , simp(A1/B, Ans, S2) , ! , writeln([S,37,A/B=Ans,succeed]) .
-simp((A+B)/C, Ans, S) :- writeln([S,38,A,B,C]) , S>0 , S2 is S-1 , simp(A/C+B/C, Ans, S2) , ! .
-simp((A-B)/C, Ans, S) :- writeln([S,39,A,B,C]) , S>0 , S2 is S-1 , simp(A/C-B/C, Ans, S2) , ! .
+simp(A*B/C, Ans) :- show([34,A]) , simp(B,B1) , simp(C,C1) , B1=C1 , simp(A, Ans) , ! .
+simp(A*B/C, Ans) :- show([35,A]) , simp(A/C,Z) , simp(Z*B, Ans) , ! .
+simp(A/B, Ans) :- show([37,A,B]) , simp(A,A1) , A\=A1 , simp(A1/B, Ans) , ! , show([37,A/B=Ans,succeed]) .
+simp((A+B)/C, Ans) :- show([38,A,B,C]) , simp(A/C+B/C, Ans) , ! .
+simp((A-B)/C, Ans) :- show([39,A,B,C]) , simp(A/C-B/C, Ans) , ! .
 
-simp(@(Exp,Var=Con), Ans, S) :- writeln([S,48]) , S>0 , S2 is S-1 , simp(Exp,Exp2,S2) , (
+simp(@(Exp,Var=Con), Ans) :- show([48]) , simp(Exp,Exp2) , (
 	Exp2 = Var , Ans = Con ;
 	atomic(Exp2) , Ans = Exp2 ;
-	Exp2 = X+Y , simp(@(X,Var=Con),X1,S2) , simp(@(Y,Var=Con),Y1,S2) , simp(X1+Y1,Ans,S2) ;
-	Exp2 = X*Y , simp(@(X,Var=Con),X1,S2) , simp(@(Y,Var=Con),Y1,S2) , simp(X1*Y1,Ans,S2) ;
-	Exp2 = X/Y , simp(@(X,Var=Con),X1,S2) , simp(@(Y,Var=Con),Y1,S2) , simp(X1/Y1,Ans,S2)
+	Exp2 = X+Y , simp(@(X,Var=Con),X1) , simp(@(Y,Var=Con),Y1) , simp(X1+Y1,Ans) ;
+	Exp2 = X*Y , simp(@(X,Var=Con),X1) , simp(@(Y,Var=Con),Y1) , simp(X1*Y1,Ans) ;
+	Exp2 = X/Y , simp(@(X,Var=Con),X1) , simp(@(Y,Var=Con),Y1) , simp(X1/Y1,Ans)
 ) , ! .
 
-simp(X, X, S):- writeln([S,52,X=X,succeed]).
+simp(X, X):- show([52,X=X,succeed]).
 
-<----(Answers,X) :- writeln("Depth Limiting") , bagof(Answer,simp(X,Answer,25),Answers).	%	Depth Limited
+<----(Answer,X) :- nb_setval(see,true) , show("Preprocessing") , preprocess(X,Answer) .	%	Preprocess
 
-<---(Answers,X) :- writeln("Preprocessing") , preprocess(X,X1) , <----(Answers,X1).	%	Preprocess
+<---(Answer,X) :- nb_setval(see,true) , <----(P,X) , show("Simplifying") , simp(P,Answer) .	%	Simplify
 
-<--(Answer,X) :- writeln("Pick Best") , <---(Answers,X) , last(Answers,Answer).	%	Pick Best
+<--(Answer,X) :- nb_setval(see,true) , <---(Simp,X) , show("Factoring") , factor(Simp,Answer) .	%	Factor
 
-<-(Answer,X) :- writeln("Factoring") , <--(Best,X) , factor(Best,Answer).	%	Factor Best
+<-(Answer,X) :- nb_setval(see,false) , preprocess(X,P) , simp(P,Simp) , factor(Simp,Answer) .	%	Quiet
+
+show(Text) :- nb_getval(see,See) , See -> writeln(Text) ; ! .
 
 factor(X+X, 2*X1) :- factor(X, X1) , ! .
 factor(N*X+X, N2*X) :- number(N) , N2 is N+1 , ! .
--- README.md
+++ README.md
@@ -4,13 +4,15 @@ Leibniz: A Rule System for Math
 
 Author:	Anthony John Ripa
 
-Date:	2018.09.20
+Date:	2018.10.20
 
 Live Demo of Version 1 at <a href='https://swish.swi-prolog.org/p/hVEWFHXN.pl'>https://swish.swi-prolog.org/p/hVEWFHXN.pl</a>
 
 Live Demo of Version 2 at <a href='https://swish.swi-prolog.org/p/JWdsRkzO.pl'>https://swish.swi-prolog.org/p/JWdsRkzO.pl</a>
 
 Live Demo of Version 3 at <a href='https://swish.swi-prolog.org/p/fMJuWNsH.pl'>https://swish.swi-prolog.org/p/fMJuWNsH.pl</a>
+
+Live Demo of Version 4 at <a href='https://swish.swi-prolog.org/p/oeuNtQbk.pl'>https://swish.swi-prolog.org/p/oeuNtQbk.pl</a>
 
 Leibniz
 --------
@@ -24,7 +26,7 @@ Consider the relatively simple (h/h)@h=0. We may proceed [h/h@h=0] = [1@h=0] = 1
 
 This bares a striking resemblance to parse trees for Context Free Grammars. You start with an expression. You apply some transformation rules to the expression. You get a new expression. However, in parse trees each node can be either a terminal or a non-terminal node. If a node is a terminal node then you can terminate and return that node as the parsed value of the original. If a node is a non-terminal node then you cannot terminate and return that node as the parsed value of the original, but you can transform that node using the transformation rules.
 
-We may use a parse tree approach to parse algebraic expressions. Instead of returning 0/0 as a terminal node, we return it as a non-terminal node. This has the same effect as not returning it at all. This results in backtracking that branch of the tree to continue to search for the answer in other parts of the tree. In simplifying h/h@h=0, we avoid returning 0/0, and end up returning 1. Similarly, in simplifying (f(x+h)-f(x))/h @ h=0, we do not return 0/0, and end up returning 2x (literally x+x).
+We may use a parse tree approach to parse algebraic expressions. Instead of returning 0/0 as a terminal node, we return it as a non-terminal node. This has the same effect as not returning it at all. This results in backtracking that branch of the tree to continue to search for the answer in other parts of the tree. In simplifying h/h@h=0, we avoid returning 0/0, and end up returning 1. Similarly, in simplifying (f(x+h)-f(x))/h @ h=0, we do not return 0/0, and end up returning 2x (literally 2*x).
 
 <pre>
   			(h@h=0)/(h@h=0)	---	0/0	
@@ -40,6 +42,6 @@ To be clear, it should be stated that <code>Leibniz</code> is not a Calculus sys
 
 One way to think about where this advantage is coming from is in terms of the properties of the transformation rules. The transformation rules of algebra as they are ordinarily cast, as well as our transformation rules, are transitive ( A transformsto B and B transformsto C implies A transformsto C ). However, in addition, the transformation rules of algebra as they are ordinarily cast, are symmetric transformation rules ( A transformsto B implies B transformsto A ) and reflexive ( A transformsto A ). So the transformsto relation for the rules of algebra as they are ordinarily cast, satisfies all the requirements of an equivalence relation. So, if A transforms to B we can just write A = B. This creates the problem. 1 = 0/0 = 0 implies 0 = 1. By not having symmetry, we avoid that problem 1 → 0/0 and 0 → 0/0 but there's nothing that 0/0 transformto. So, we never get 0 → 1 or 1 → 0 or 0=1.
 
-To expand on this, we might consider the problem of simplifying 0/0. If 0/0 is a non-terminal, and the replacement rules can never transform it into a terminal node, then we would have an expression that cannot be parsed into anything (including itself). One solution is to allow 0/0 as a terminal. This might obscure other solutions if there are any so instead of returning the first terminal we could return the list of all terminals. In the case of parsing 0/0 we get the list [0/0]. In case of parsing h/h@h=0 we get the list [0/0,1]. As the parse order always tries to evaluate @ first the 0/0 will always be the earlier solution. We can return the last solution as the canonical solution. So, 0/0 parses to 0/0, and h/h@h=1 parses to 1.
+To expand on this, we might consider the problem of simplifying 0/0. If 0/0 is a non-terminal, and the replacement rules can never transform it into a terminal node, then we would have an expression that cannot be parsed into anything (including itself). One solution is to allow 0/0 as a terminal. This might obscure other solutions if there are any so instead of returning the first terminal we could return the list of all terminals. In the case of parsing 0/0 we get the list [0/0]. In case of parsing h/h@h=0 we get the list [0/0,1]. As the parse order always tries to evaluate @ first the 0/0 will always be the earlier solution. We can return the last solution as the canonical solution. So, 0/0 parses to 0/0, and h/h@h=0 parses to 1.
 
 Furthermore, if we are always going to return the canonical solution, we might as well save time and calculate a smaller parse tree. When calculating h/h@h=0, we might as well always simplify the expression to the left of the @ symbol first (in this case h/h simplifies to 1) then apply the @h=0 (in this case 1@h=0) to get the answer (in this case 1). This way we only return 0/0 when required (for example, if the input were literally 0/0, or if the input were something like 0/0@h=0).
